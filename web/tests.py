6d1cac3ef2599938e7018e47b2f4228edebe4032
Tristan Mangeard <tmangeard@gmx.fr>
2019-10-01 08:00:22 +1100

    fisrt test

--- web/tests.py
+++ web/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.urls import reverse
+
+class IndexPageTestCase(TestCase):
+    def test_index_page(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
